4f8cc49b4bcf216235488054c40633bb8e7da072
Probir-Ray <somironroy.need@gmail.com>
2021-10-19 22:02:56 +0600

    update

--- src/components/Contact/Address/Address.js
+++ src/components/Contact/Address/Address.js
@@ -2,29 +2,37 @@ import React from 'react';
 
 const Address = () => {
     return (
-        <div>
-            <form className="row g-3">
-            <div className="col-md-6">
-                <label for="inputPassword4" className="form-label">Name</label>
-                <input type="text" className="form-control" />
+        <div className="my-5 row text-center">
+            <div className="card card-default col-md-5 bg-primary mb-5 text-white">
+                <div className="card-header">BD Address</div>
+                <div className="card-body">
+                    <h5>Phone: +88017-666333</h5>
+                    <p>Address: LRK, Gulshan, Dhaka</p>
+                </div>
             </div>
-            <div className="col-md-6">
-                <label for="inputEmail4" className="form-label">Email</label>
-                <input type="email" className="form-control" id="inputEmail4"/>
+            <div className="col-1"></div>
+            <div className="card card-default col-md-5 bg-danger text-white mb-5">
+                <div className="card-header">USA Address</div>
+                <div className="card-body">
+                    <h5>Phone: +993-666333</h5>
+                    <p>Address: 113 Street, Manhaton, Newyork</p>
+                </div>
             </div>
-            
-            <div className="col-12">
-                <label for="inputAddress" className="form-label">Address</label>
-                <input type="text" className="form-control" id="inputAddress" placeholder="Hsk Tower, Dhaka"/>
+            <div className="card card-default col-md-5 bg-info text-white mb-5">
+                <div className="card-header">UK Address</div>
+                <div className="card-body">
+                    <h5>Phone: +731-666333</h5>
+                    <p>Address: 64 Cantbery, Landon</p>
+                </div>
             </div>
-            <div className="col-md-6">
-                <label for="inputCity" className="form-label">City</label>
-                <input type="text" className="form-control" id="inputCity"/>
+            <div className="col-1"></div>
+            <div className="card card-default col-md-5 bg-primary text-white">
+                <div className="card-header">UAE Address</div>
+                <div className="card-body">
+                    <h5>Phone: +234-666333</h5>
+                    <p>Address: 11 Dubai City, Dubai</p>
+                </div>
             </div>
-            <div className="col-12">
-                <button type="submit" className="btn btn-primary">Sign in</button>
-            </div>
-            </form>            
         </div>
     );
 };
--- src/components/Contact/Contact/Contact.js
+++ src/components/Contact/Contact/Contact.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Address from '../Address/Address';
+import ContactForm from '../ContactForm/ContactForm';
 
 const Contact = () => {
     return (
         <div className='container my-5'>
             <h2 className='text-center my-3'>Contact Us</h2>
+            <ContactForm></ContactForm>
+            <h2 className='text-center mt-5 mb-3'>Address</h2>
             <Address></Address>
         </div>
     );
